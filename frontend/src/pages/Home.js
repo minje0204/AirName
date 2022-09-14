17fa0af77448116976e2b1e1a8beb06da5810ba3
정지은 <lisajieun@naver.com>
2022-09-15 01:20:31 +0900

    [#S07P22A108-142] feat: Home 레이아웃 완성

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,25 +1,64 @@
 import React from "react";
 import AirplaneWindow from "./../components/home/AirplaneWindow.js";
 import StartBtn from "./../components/home/StartBtn.js";
+import styled from "styled-components";
 
 function Home() {
   return (
-    <div>
-      <div>Home</div>
+    <StyledWrapper>
       <AirplaneWindow />
-      <StartBtn
-        title={"영어 이름 추천받기"}
-        subtitle={"영어 이름이 없다면? 🙅‍♀️"}
-        to={"/entry"}
-      />
-      <br />
-      <StartBtn
-        title={"내 이름 레포트 보기"}
-        subtitle={"이미 영어 이름이 있어요 ! 🙆‍♂️ "}
-        to={"/finReport"}
-      />
-    </div>
+      <div id="right-container">
+        <div>
+          <img src="/logo.png" alt="logo" width={"300px"} />
+        </div>
+        <div id="button-container">
+          <div>
+            <StartBtn
+              title={"영어 이름 추천받기"}
+              subtitle={"영어 이름이 없다면? 🙅‍♀️"}
+              to={"/entry"}
+            />
+          </div>
+          <div>
+            <StartBtn
+              title={"내 이름 레포트 보기"}
+              subtitle={"이미 영어 이름이 있어요 ! 🙆‍♂️ "}
+              to={"/finReport"}
+            />
+          </div>
+        </div>
+      </div>
+    </StyledWrapper>
   );
 }
 
 export default Home;
+
+const StyledWrapper = styled.div`
+  @media (min-width: 650px) {
+    min-width: 630px;
+  }
+  @media (max-width: 650px) {
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
+
+  display: flex;
+  height: 100vh;
+  justify-content: space-between;
+  align-items: center;
+
+  #right-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 300px;
+  }
+
+  #button-container {
+    height: 250px;
+    display: flex;
+    justify-content: space-evenly;
+    flex-direction: column;
+  }
+`;
