281ee1681df0fa10646ab322efac52838474ef6b
정지은 <lisajieun@naver.com>
2022-09-16 00:04:19 +0900

    [#S07P22A108-138] chore: MUI 컬러팔레트 설정

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,15 +1,15 @@
 import { createTheme } from "@mui/material/styles";
-import { purple } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
     primary: {
-      // Purple and green play nicely together.
-      main: purple[500],
+      light: "#8DC7F7",
+      main: "#42a5f5",
     },
-    secondary: {
-      // This is green.A700 as hex.
-      main: "#11cb5f",
+    info: {
+      light: "#F9F7F4",
+      main: "#FFFCBF",
+      dark: "#FECAF8",
     },
   },
 });
