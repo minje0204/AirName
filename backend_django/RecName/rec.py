4bfcd36077ba2a55ad471e4355ae6a5f5f03dca6
rotter9027@gmail.com <rotter9027@gmail.com>
2022-09-15 16:53:55 +0900

    [#S07P22A108-143] chore: 추천 결과를 json배열로 반환하도록 수정

--- backend_django/RecName/rec.py
+++ backend_django/RecName/rec.py
@@ -44,6 +44,19 @@ def JaroDistance(data, name):
     sim = jellyfish.jaro_distance(data,NysCode)
     return sim
 
+def Filter(dataframes):
+    #연령대 필터
+    #1안. 내 나이에서 멀어질수록 가중치가 낮아짐
+    #이 경우
+    #2안. 내 나이 +-3년 안에 있는 데이터만 이용
+
+    #성별 필터
+    #설문조사에서 응답한 값에 따라 데이터셋 자체에서 필터링
+
+    #희귀도 필터
+    #연령대, 성별 필터를 거친 후 추천도가 같은 배열에 대하여 희귀도 순으로 정렬
+
+    return 0
 
 def Recommend(kor_name):
     data = LoadDataframes()
@@ -61,6 +74,8 @@ def Recommend(kor_name):
     #유사도 기준 정렬
     df_sim = df_sim.sort_values('nysiis_sim',ascending=False)
 
-    name_array = df_sim['name'].to_numpy()
+    name_array = df_sim['name'].head(4).to_numpy()
+    #list를 dict로 바꿔야 Json으로 변환할 수 있다. (Front에 Json으로 리턴해주기 위함)
+    name_array = dict(enumerate(name_array))
 
     return name_array
--- backend_django/RecName/views.py
+++ backend_django/RecName/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from rest_framework.response import Response
 from .models import NameData
@@ -21,12 +23,15 @@ class NameListAPI(APIView):
     def get(self, request):
         #Json 파일로 받아야 하지만 테스트를 위해 현재 한글이름 string값 받아서 처리하도록 함
         #data = JSONParser().parse(request)
-        kor_name = request.GET.get('KorName', None)
-        arr = Recommend(kor_name)
-        print(arr)
-        
+        #kor_name = request.GET.get('KorName', None)
+
+        arr = Recommend(request.data['name'])
+        print(request.data['gender'])
+        print(request.data['age'])
+
         #배열을 Json파일로 변환 후 넘겨줘야 함
         #serializer = RecSerializer(data=data)
+        data = json.dumps(arr)
 
-        return Response()
+        return JsonResponse(data, safe=False)
 
