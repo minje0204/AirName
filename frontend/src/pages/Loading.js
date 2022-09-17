ab4e4a002b1de8b84d5a866c34eb1802483b745d
정지은 <lisajieun@naver.com>
2022-09-18 01:39:11 +0900

    [#S07P22A108-171] chore: Loading tmi box 위치 변경

--- frontend/src/pages/Loading.js
+++ frontend/src/pages/Loading.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import LoadingTMI from '../components/loading/LoadingTMI';
 import ProgressBar from '../components/loading/LoadingProgressBar';
 
 export default function Loading() {
   return (
-    <div>
+    <StyledWrapper>
       <LoadingTMI />
       <ProgressBar />
-    </div>
+    </StyledWrapper>
   );
 }
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
