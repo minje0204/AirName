46295d84b6da64dd99dcbaca9d0785188f6f6843
2riing <5032303000@naver.com>
2022-09-17 20:52:51 +0900

    fix: import delete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Provider } from 'react-redux';
 import './index.css';
 import store from './redux/configStore';
 
