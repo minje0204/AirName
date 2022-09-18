51db76cb81f645d9e7cfe42d382fdebe5ac184bf
정지은 <lisajieun@naver.com>
2022-09-18 23:47:30 +0900

    [#S07P22A108-150] chore: Entry Warning 삭제

--- frontend/src/pages/EntryEn.js
+++ frontend/src/pages/EntryEn.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import InputAlert from '../components/entry/EntryCardWarning';
 import EntryCardEn from '../components/entry/EntryCardEn';
 
 function EntryEn() {
   return (
     <div>
-      <InputAlert />
       <EntryCardEn />
     </div>
   );
