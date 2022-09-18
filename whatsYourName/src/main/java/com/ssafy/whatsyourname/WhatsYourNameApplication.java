f3449667d9d99645273d91e683ab90fcdeeb4cc1
윤여빈 <tommy7899@naver.com>
2022-09-18 18:45:59 +0900

    [#S07P22A108-159] feat: WhatsYourNameApplication.java 추가

---
+++ whatsYourName/src/main/java/com/ssafy/whatsyourname/WhatsYourNameApplication.java
@@ -0,0 +1,14 @@
+package com.ssafy.whatsyourname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsYourNameApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsYourNameApplication.class, args);
+    }
+
+}
