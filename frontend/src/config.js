f0093e8f25c365983e4e6e1f2fa07bff6087d70c
soohong <5032303000@naver.com>
2022-09-19 12:11:29 +0900

    fix: api url 수정

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,18 +2,10 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 const API = {
   POST: `${BASE_URL}/posts`,
-  COMMENT: `${BASE_URL}/comments`,
-  MAIN: `${BASE_URL}/main`,
-  MYPAGE: `${BASE_URL}/users/private/user`,
-  USER_PAGE: `${BASE_URL}/users/public/user`,
-  SIGNUP: `${BASE_URL}/users/signup`,
-  WRITERDATA: `${BASE_URL}/users/?user_tag_id=`,
-  TAGDATA: `${BASE_URL}/branch_tags/userTagList`,
-  DITAILLIST: `${BASE_URL}/postings`,
-  KEYWORDS: `${BASE_URL}/keywords/list`,
-  LOGIN: `${BASE_URL}/users/signin`,
-  DETAIL_PAGE: `${BASE_URL}/postings/detail`,
-  RELATED: `${BASE_URL}/postings`
+  ENTRY: `${BASE_URL}/rec/sound`,
+  SURVEY: `${BASE_URL}/rcmnd`,
+  SELETED: `${BASE_URL}/selected`,
+  FINREPORT: `${BASE_URL}/report`,
 };
 
 export default API;
