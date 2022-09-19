529c42159b75c5126b9927df0aa0a26cc55cd400
김상협 <rotter9027@gmail.com>
2022-09-19 14:07:54 +0900

    chore : CORS 수정

--- backend_django/YourName/settings.py
+++ backend_django/YourName/settings.py
@@ -54,6 +54,9 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = False
+
 ROOT_URLCONF = 'YourName.urls'
 
 TEMPLATES = [
