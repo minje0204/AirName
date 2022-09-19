3fb4ffc49349bda332d71c6dc01876b5f9f93a53
정지은 <lisajieun@naver.com>
2022-09-19 16:22:31 +0900

    [#S07P22A108-194] chore: Loading 버튼 css 수정

--- frontend/src/components/loading/LoadingCompleteBtn.js
+++ frontend/src/components/loading/LoadingCompleteBtn.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
-export default function LoadBtn() {
+export default function LoadBtn({ btnVisiblity }) {
   return (
     <Button
       variant="contained"
@@ -10,6 +10,10 @@ export default function LoadBtn() {
       size="large"
       component={Link}
       to="/rcmnd"
+      style={{
+        visibility: btnVisiblity,
+        marginTop: '10px'
+      }}
     >
       <span>요정이 추천해준 이름 보러가기</span>
     </Button>
