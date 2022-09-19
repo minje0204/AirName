ba51e547a050c989d906444db2bc8451244500a3
soohong <5032303000@naver.com>
2022-09-19 10:38:09 +0900

    test: tts btn 주석처리

--- frontend/src/components/rcmnd/TicketBack.js
+++ frontend/src/components/rcmnd/TicketBack.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 // import Button from '@mui/material/Button';
 import TicketHeadAirplane from '../../asset/img/rcmnd/TicketHeadAirplane.png';
-import TicketTTSBtn from './TicketTTSBtn';
+// import TicketTTSBtn from './TicketTTSBtn';
 import './Rcmnd.css';
 
 function TicketFront({ name }) {
@@ -22,7 +22,7 @@ function TicketFront({ name }) {
           <div>
             <TickeNameContainer>
               {name}
-              <TicketTTSBtn username={name} />
+              {/* <TicketTTSBtn username={name} /> */}
             </TickeNameContainer>
           </div>
           <SelectBtnContainer>
