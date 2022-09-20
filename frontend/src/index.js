325bb2edfd0bb53338199315f154b33e4b90556d
2riing <5032303000@naver.com>
2022-09-21 01:16:26 +0900

    [#S07P22A108-263] feat: reat GA init
    Former-commit-id: 53eeab4439e6bea8d05a1452e5cd28ffcb5f038a

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 // 카카오톡 공유하기 API KEY setting
 const KAKAO_KEY = process.env.REACT_APP_KAKAO_API_KEY;
 window.Kakao.init(KAKAO_KEY);
+
+// GA ID setting
+const TRACKING_ID = process.env.	REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
+RaactGA.initialize(TRACKING_ID);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
