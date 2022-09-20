4735219aebbf5c4555b4c2258acf2e56634333cb
2riing <5032303000@naver.com>
2022-09-20 19:03:45 +0900

    fix: question ans anskey 파일 수정
    Former-commit-id: be522b1631f5022e7ce2baf23bd02fb8706e6fe6

--- frontend/src/components/survey/AnswerKey.js
+++ frontend/src/components/survey/AnswerKey.js
@@ -1,7 +1,7 @@
 const answerKey = [
   'Gender', 'OldFashionedness','Oldness', 'Formality', 
   'Class', 'Urban-rural', 'Truthfulness', 'Extremly', 
-  'Roughness','Strangeness', 'Complexness', 'Seriousness', ''
+  'Roughness','Strangeness', 'Complexness', 'Seriousness'
 ];
 
 export default answerKey;
