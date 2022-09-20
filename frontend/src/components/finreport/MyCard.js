1b828307744a60b407e2d898030c06592e9892d4
2riing <5032303000@naver.com>
2022-09-20 14:17:59 +0900

    [#S07P22A108-200] feat: finreport 티켓 그리기
    Former-commit-id: c3e75c1edc8a4c38afb0f12eabdb2f6b587bdcbf

--- frontend/src/components/finreport/MyCard.js
+++ frontend/src/components/finreport/MyCard.js
@@ -1,22 +1,196 @@
 import React from 'react';
 import styled from 'styled-components';
-import SampleTicket from '../../asset/img/finreport/SampleTicket.png';
+import QRImg from '../../asset/img/finreport/QRImg.png';
+import Barcode from '../../asset/img/finreport/Barcode.png';
+import Airplane from '../../asset/img/finreport/Airplane.png';
 
 function MyCard() {
   return (
-    <MyCardContainter>
-      <MyCardImg src={SampleTicket} alt="MyTicket" />
-    </MyCardContainter>
+    <TicketWrapper>
+      <TicketHead>
+        <TicketLeftHead>AIR NAME</TicketLeftHead>
+        <TicketRightHead>Right</TicketRightHead>
+      </TicketHead>
+      <TicketBody>
+        <TicketLeft>
+          <PassangerInfo>
+            <Passanger>
+              <PassangerTitle>Name of Passenger</PassangerTitle>
+              <PassangerBody>Tommy</PassangerBody>
+            </Passanger>
+            <Seat>
+              <SeatTitle>SEAT</SeatTitle>
+              <SeatBody>108A</SeatBody>
+            </Seat>
+            <Gate>
+              <GateTitle>GATE</GateTitle>
+              <GateBody>7</GateBody>
+            </Gate>
+          </PassangerInfo>
+          <BarcodeContainer>
+            <img id='barcode-img' src={Barcode} />
+          </BarcodeContainer>
+        </TicketLeft>
+        <TicketRight>
+          <TicketJourney>
+            <Depart>KOR</Depart>
+            <AirplaneImg>
+              <img id='airplane-img' src={Airplane} />
+            </AirplaneImg>
+            <Arrive>ATL</Arrive>
+            <QR>
+              <img id='qr-img' src={QRImg} />
+            </QR>
+          </TicketJourney>
+        </TicketRight>
+      </TicketBody>
+    </TicketWrapper>
   );
 }
 
 export default MyCard;
-const MyCardContainter = styled.div`
+const TicketWrapper = styled.div`
   display: flex;
   justify-content: center;
-  color: blue;
+  align-items: center;
+  flex-direction: column;
+  width: 700px;
+  height: 300px;
 `;
 
-const MyCardImg = styled.img`
-  width: 80%;
+const TicketHead = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20%;
+  width: 100%;
+`;
+const TicketBody = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80%;
+  width: 100%;
+`;
+
+const TicketLeftHead = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--primaryDark);
+  color: white;
+  border-top-left-radius: 15px;
+  width: 70%;
+  height: 100%;
+`;
+
+const TicketRightHead = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--primaryDark);
+  color: white;
+  border-top-right-radius: 15px;
+  border-left: 1px dashed black;
+  width: 30%;
+  height: 100%;
+`;
+
+const TicketLeft = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: white;
+  border-bottom-left-radius: 15px;
+  width: 70%;
+  height: 100%;
+`;
+const TicketRight = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  border-bottom-right-radius: 15px;
+  border-left: 1px dashed black;
+  width: 30%;
+  height: 100%;
+`;
+const PassangerInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Passanger = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 10px;
+`;
+const PassangerTitle = styled.div`
+  font-size: 20px;
+  font-family: 'SCDream3';
+`;
+const PassangerBody = styled.div`
+  font-size: 40px;
+  font-family: 'SCDream7';
+`;
+const Seat = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 10px;
+`;
+const SeatTitle = styled.div`
+  font-size: 20px;
+  font-family: 'SCDream3';
+`;
+const SeatBody = styled.div`
+  font-size: 40px;
+  font-family: 'SCDream7';
+`;
+const Gate = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 10px;
+
+`;
+const GateTitle = styled.div`
+  font-size: 20px;
+  font-family: 'SCDream3';
+`;
+const GateBody = styled.div`
+  font-size: 40px;
+  font-family: 'SCDream7';
+`;
+const BarcodeContainer = styled.div`
+  margin-top: 20px;
+`;
+
+const TicketJourney = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Depart = styled.div`
+font-size: 20px;
+`;
+const AirplaneImg = styled.div`
+
+#airplane-img{
+  width: 30px;
+}
+`;
+const Arrive = styled.div`
+font-size: 20px;`;
+const QR = styled.div`
+margin-top: 10px;
+#qr-img{
+  width: 90px;  
+}
 `;
