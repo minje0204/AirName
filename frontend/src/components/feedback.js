b98347e653bebcbc6507d573477b6b3e085ef372
정지은 <lisajieun@naver.com>
2022-09-20 16:10:12 +0900

    [#S07P22A108-213] chore: 말풍선 css 및 구글폼 주소 수정

--- frontend/src/components/feedback.js
+++ frontend/src/components/feedback.js
@@ -5,7 +5,6 @@ import elf from '../asset/img/feedback/Elf.png';
 
 function FeedbackBtn() {
   const [sbOn, setSbOn] = useState(false);
-
   return (
     <StyledWrapper>
       {sbOn ? (
@@ -20,7 +19,7 @@ function FeedbackBtn() {
         variant="contained"
         target="_blank"
         id="fbBtn"
-        href="https://forms.gle/YWC4eXbDrLLCAgTv9"
+        href="https://forms.gle/adf2ZUpUiFUCo8cC9"
         onMouseOver={() => {
           setSbOn(true);
         }}
@@ -38,28 +37,36 @@ export default FeedbackBtn;
 
 const StyledWrapper = styled.div`
   #elfImg {
-    width: 7vmin;
     height: 7vmin;
+    min-height: 20px;
+    max-height: 40px;
+    aspect-ratio: 1 / 1;
   }
   #fbBtn {
+    aspect-ratio: 1 / 1;
+    height: 14vmin;
+    min-width: 40px;
+    min-height: 40px;
+    max-width: 80px;
+    max-height: 80px;
+    padding: 0;
+    position: fixed;
+    right: 3vmin;
+    bottom: 3vmin;
     background-color: var(--background);
     box-shadow: 3px 3px 5px rgba(0, 0, 0, 25%);
     border-radius: 50%;
     border: 2px solid black;
-    width: 12vmin;
-    height: 12vmin;
-    position: fixed;
-    right: 3vmin;
-    bottom: 3vmin;
   }
   .speechBubble {
     position: fixed;
-    right: 18vmin;
+    right: clamp(65px, 20vmin, 120px);
     bottom: 3vmin;
-    padding: 15px;
+    padding: 10px;
     border-radius: 20px;
     background: var(--infoDark);
     border-radius: 0.4em;
+    font-size: clamp(0.5rem, 2vw, 1rem);
   }
   .speechBubble:after {
     content: '';
