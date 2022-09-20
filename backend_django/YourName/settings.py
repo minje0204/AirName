9855e6a65e072ebe880f275afc9e505fecca7161
minje0204 <minje@ajou.ac.kr>
2022-09-20 15:34:21 +0900

    chore: add allowed_host

--- backend_django/YourName/settings.py
+++ backend_django/YourName/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'trl*efvpfmoc9e1x*5dii$g1n($+qd-ig9%!hd$(^!y5%@z1q*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.35.169.128']
 
 
 # Application definition
