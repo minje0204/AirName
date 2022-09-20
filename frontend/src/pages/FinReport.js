9399c0465f8adb45a0bcad6b223f9e3b08abf27c
2riing <5032303000@naver.com>
2022-09-20 20:13:38 +0900

    [#S07P22A108-211] feat: finreport 통신, 저장
    Former-commit-id: c3d855258e9dab538eebb16951c17e418e55cbb8

--- frontend/src/pages/FinReport.js
+++ frontend/src/pages/FinReport.js
@@ -1,19 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
+
 import FinBtns from '../components/finreport/FinBtns';
 import FinTitle from '../components/finreport/FinTitle';
 import MyCard from '../components/finreport/MyCard';
 import ReportContent from '../components/finreport/ReportContent';
+import API from '../config';
 
 function FinReport() {
   const hometown = 'Korea';
   const { username } = useParams();
+  const birth = localStorage.getItem('birth');
+  const gender = localStorage.getItem('gender');
+  // const [reportData, setReportData] = useState({})
+  const [femaleMeaning, setFemaleMeaning] = useState('')
+  const [femaleState, setFemaleState] = useState('')
+  const [maleMeaning, setMaleMeaning] = useState('')
+  const [maleState, setMaleState] = useState('')
+  
+  const [mainState, setMainState] = useState('')
+
+  // const calcMainState = {
+  //   if (maleState && femaleState) {
+
+  //   }
+  // }
+
+  const getReportData = async () => {
+  await axios
+    .get(`${API.FINREPORT}/${username}/${gender}/${birth}`)
+    .then((res) => {
+      setFemaleMeaning(res.data.female.meaning);
+      setFemaleState(res.data.female.state);
+      setMaleMeaning(res.data.male.meaning);
+      setMaleState(res.data.male.state);
+    });
+  };
+  useEffect(() => {
+    getReportData()
+  }, [])
+    
   return (
     <StyledWrapper>
       <FinTitle username={username} hometown={hometown} />
       <FinBtns />
-      <MyCard username={username} hometown={hometown}/>
-      <ReportContent username={username} hometown={hometown} />
+      <MyCard username={username} hometown={hometown} />
+      <ReportContent username={username} report={hometown} />
     </StyledWrapper>
   );
 }
