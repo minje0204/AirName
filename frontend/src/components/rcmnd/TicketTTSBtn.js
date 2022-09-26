1b76ff0689b8bebcc3de2a353954082d4061acf4
2riing <5032303000@naver.com>
2022-09-26 16:04:26 +0900

    [#S07P22A108-354] fix: ios tts voice 설정, tts 컴포 하나로 리팩토링
    Former-commit-id: e9d9ed12589b67c00392a747c6b1500a4d66fb5b

--- frontend/src/components/rcmnd/TicketTTSBtn.js
+++ frontend/src/components/rcmnd/TicketTTSBtn.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 import SpeakerImg from '../../asset/img/finreport/Speaker.svg';
 
-function TTSBtn(props) {
+function TTSBtn({username}) {
   const synth = window.speechSynthesis;
   const msg = new SpeechSynthesisUtterance();
+  const voicesList = window.speechSynthesis.getVoices()
   // 언어, 속도 설정
 
 
@@ -18,7 +19,9 @@ function TTSBtn(props) {
     // 이름 변수 넣기
     msg.lang = 'en-US';
     msg.rate = 0.8;
-    msg.text = props.username;
+    msg.text = username;
+    // voice 속성 설정
+    msg.voice = voicesList.find((voice) => voice.lang === 'en-US')
     synth.speak(msg);
   }
 
