d6cd9b8876082eac438d096d2728aa2165b6788a
정지은 <lisajieun@naver.com>
2022-09-28 17:13:05 +0900

    [#S07P22A108-344] fix: Entry Ko, En 잘못된 한글이름(성) 처리
    Former-commit-id: 49a5ff5bbd43bf85f2fac25ace7428315fb867ac

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -6,7 +6,8 @@ const API = {
   SURVEY: `${BASE_URL}/rcmnd`,
   SELETED: `${BASE_URL}/selected`,
   FINREPORT: `${BASE_URL}/report`,
-  LOADING: `${BASE_URL}/loading/`
+  LOADING: `${BASE_URL}/loading/`,
+  NAMECHECK: `${BASE_URL}/rec/check`
 };
 
 export default API;
