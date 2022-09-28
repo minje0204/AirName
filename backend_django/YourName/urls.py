78daf6c356c51b2aff169329b11da68fe8285286
윤여빈 <tommy7899@naver.com>
2022-09-28 11:14:16 +0900

    [#S07P22A108-289] feat: CheckName api 개발
    Former-commit-id: 9d5bbf6d5ba9d848dad3fb3ff6491802aededb67

--- backend_django/YourName/urls.py
+++ backend_django/YourName/urls.py
@@ -4,11 +4,12 @@ from django.urls import path, include
 from RecName.views import NameList
 from FinalReport.views import GetReport
 from LoadingLabeling.views import Labeling
+from ValidateName.views import ValidateName
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rec/', include('RecName.urls')),
     path('report/<str:name>/<int:birth>', GetReport.as_view()),
-
+    path('validate/<str:name>/<int:birth>',ValidateName.as_view()),
     path('loading/',Labeling.as_view())
 ]
