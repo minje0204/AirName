d4a18055a1198d76c3ea23fb0372a297dfae3463
윤여빈 <tommy7899@naver.com>
2022-09-29 19:54:39 +0900

    [#S07P22A108-317] feat: 연도별 추이 API (GetNamePerYearList) 개발
    Former-commit-id: d78860fadec43e60467f8abe72061a315663ed64

--- backend_django/NameYear/views.py
+++ backend_django/NameYear/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
+
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from .year import GetNamePerYear
+class GetNamePerYearList(APIView):
+    def get(self,request,**kwargs):
+        result = GetNamePerYear(kwargs['name'])
+
+        data = json.dumps(result)
+
+        return JsonResponse(data, safe=False)
--- backend_django/YourName/urls.py
+++ backend_django/YourName/urls.py
@@ -1,7 +1,7 @@
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
-from RecName.views import NameList
+from NameYear.views import GetNamePerYearList
 from FinalReport.views import GetReport
 from LoadingLabeling.views import Labeling
 from SpeakName.views import SpeakName
@@ -14,7 +14,8 @@ urlpatterns = [
     path('report/<str:name>/<int:birth>', GetReport.as_view()),
     path('validate/<str:name>/<int:birth>',ValidateName.as_view()),
     path('loading/',Labeling.as_view()),
-    path('speaking/<str:name>',SpeakName.as_view())
+    path('speaking/<str:name>',SpeakName.as_view()),
+    path('year/<str:name>', GetNamePerYearList.as_view())
 ]
 
 
