ab87809e536f5de076732369c56700cdcbaa4648
minje0204 <minje@ajou.ac.kr>
2022-10-03 18:27:19 +0900

    feat: usaMap넣기
    Former-commit-id: 83e911c0dc60bb339876e9d3269eb4f0036712a3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,10 +15,13 @@ import {
 } from './pages';
 import FeedbackBtn from './components/feedback';
 import theme from './theme';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
