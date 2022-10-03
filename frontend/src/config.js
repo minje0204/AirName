afba32fdc38202a722ff90f2e4618a40397d664a
rotter9027@gmail.com <rotter9027@gmail.com>
2022-10-03 17:01:35 +0900

    chore:path 로컬 테스트용에서 수정
    Former-commit-id: 7180fa65afbbcc7f343d5395238a0c9c62bf5a28

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,4 @@
-// const BASE_URL = 'https://airname.shop/api';
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'https://airname.shop/api';
 
 const API = {
   POST: `${BASE_URL}/posts`,
