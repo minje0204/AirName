3771692ba38474d971d800f914d0195bcec95a6b
2riing <5032303000@naver.com>
2022-10-03 16:18:14 +0900

    feat: 프로그래스바 초기 생성
    Former-commit-id: 31f7a575bcf83c4ce2b63f9a360d2d275960e8f5

--- frontend/src/components/survey/SurveySection.js
+++ frontend/src/components/survey/SurveySection.js
@@ -105,7 +105,13 @@ function SurveySection() {
   }, []);
 
   return (
-    <SveySectionContainer>
+    <SveySectionContainer>    
+      
+      <Container>
+      {/*%로 부모넓이의 1/5 씩 넓어짐*/}
+      <Progress width={(10/5)*100 + "%"}/>
+      <Dot/>
+      </Container>
       <SveyHead>
         <SveyImg>
           <img id="survey-img" src={Fairly} />
@@ -140,6 +146,34 @@ const SveySectionContainer = styled.div`
   width: 90vw;
   max-width: 750px; ;
 `;
+
+const Container = styled.div`
+  margin: 50px auto;
+  background-color: #eee;
+  width: 500px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  border-radius: 20px;
+`;
+const Progress = styled.div`
+  background-color: blue;
+  width: ${props => props.width};
+  height: 100%;
+  transition: width 1s;
+  border-radius: 20px;
+`;
+
+//프로그레스 바에 원 달아서 프로그레스 바가 차오를 때 같이 차오름
+const Dot = styled.div`
+  width: 70px;
+  height: 70px;
+  box-sizing: border-box;
+  border: 10px solid blue;
+  border-radius: 35px;
+  background: yellow;
+  margin-left: -35px;
+`
 
 const SveyHead = styled.div`
   display: flex;
